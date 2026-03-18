b94ce682c36de6ad296286c5296c61599ed8f632
yansanity1998 <byanyan1998@gmail.com>
2026-03-18 11:06:05 +0800

    feat: Implement Firebase authentication and Firestore integration, and add `@react-native-async-storage/async-storage` dependency.

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,11 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBYaecHr1dROaew2PGOZZWkPzTHK5Ft0I",
   authDomain: "smartchef-ai-9fe5d.firebaseapp.com",
@@ -19,7 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
+// Initialize Firebase Authentication
 export const auth = getAuth(app);
 
 // Initialize Cloud Firestore and get a reference to the service
